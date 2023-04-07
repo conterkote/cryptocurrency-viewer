97c07fe96f5a64461c9e7b195b81b9935729656e
conterkote <conterkote@mail.ru>
2023-04-08 04:04:36 +0700

    Fix logo src

--- src/components/main/Header.tsx
+++ src/components/main/Header.tsx
@@ -15,7 +15,7 @@ function Header({name}: IHeaderProps) {
           <div className={`container w-full m-auto flex justify-between items-center text-white`}>
             <div className="flex items-center">
               <div className="logo-name flex items-center mr-5 border-r-2 pr-5 border-r-dark-sub">
-                <img src={"./src/assets/logo.svg"} alt="404" className="w-8 mr-3"/>
+                <img src={"https://drive.google.com/u/0/uc?id=1bNDoZeEEFqyDvJemihOyBArhmhtDjRfN&export=download"} alt="404" className="w-8 mr-3"/>
                 <p className="text-[18px]">{name}</p>
               </div>
               <div className="header-items flex items items-center text-[18px]">
